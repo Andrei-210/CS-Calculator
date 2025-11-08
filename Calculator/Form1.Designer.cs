43d7822df65161acbb8ac1df87001a919d92f1ae
Andrei <andreinicoli21@gmail.com>
2025-11-08 20:55:47 +0200

    Initial commit

---
+++ Calculator/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.PnlTitle = new System.Windows.Forms.Panel();
+            this.PnlHistory = new System.Windows.Forms.Panel();
+            this.BtnExit = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.BtnMenu = new System.Windows.Forms.Button();
+            this.BtnHistory = new System.Windows.Forms.Button();
+            this.TxtDisplay2 = new System.Windows.Forms.TextBox();
+            this.TxtDisplay1 = new System.Windows.Forms.TextBox();
+            this.BtnClearHistory = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.artanElipseControl1 = new Calculator.ArtanElipseControl();
+            this.PnlTitle.SuspendLayout();
+            this.PnlHistory.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PnlTitle
+            // 
+            this.PnlTitle.Controls.Add(this.button2);
+            this.PnlTitle.Controls.Add(this.button1);
+            this.PnlTitle.Controls.Add(this.BtnExit);
+            this.PnlTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.PnlTitle.Location = new System.Drawing.Point(0, 0);
+            this.PnlTitle.Margin = new System.Windows.Forms.Padding(0);
+            this.PnlTitle.Name = "PnlTitle";
+            this.PnlTitle.Size = new System.Drawing.Size(350, 40);
+            this.PnlTitle.TabIndex = 0;
+            // 
+            // PnlHistory
+            // 
+            this.PnlHistory.Controls.Add(this.richTextBox1);
+            this.PnlHistory.Controls.Add(this.BtnClearHistory);
+            this.PnlHistory.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.PnlHistory.Location = new System.Drawing.Point(0, 565);
+            this.PnlHistory.Margin = new System.Windows.Forms.Padding(0);
+            this.PnlHistory.Name = "PnlHistory";
+            this.PnlHistory.Size = new System.Drawing.Size(350, 5);
+            this.PnlHistory.TabIndex = 1;
+            // 
+            // BtnExit
+            // 
+            this.BtnExit.Dock = System.Windows.Forms.DockStyle.Right;
+            this.BtnExit.FlatAppearance.BorderSize = 0;
+            this.BtnExit.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.BtnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnExit.Image = ((System.Drawing.Image)(resources.GetObject("BtnExit.Image")));
+            this.BtnExit.Location = new System.Drawing.Point(300, 0);
+            this.BtnExit.Margin = new System.Windows.Forms.Padding(0);
+            this.BtnExit.Name = "BtnExit";
+            this.BtnExit.Size = new System.Drawing.Size(50, 40);
+            this.BtnExit.TabIndex = 0;
+            this.BtnExit.UseVisualStyleBackColor = true;
+            this.BtnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(250, 0);
+            this.button1.Margin = new System.Windows.Forms.Padding(0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(50, 40);
+            this.button1.TabIndex = 1;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(200, 0);
+            this.button2.Margin = new System.Windows.Forms.Padding(0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(50, 40);
+            this.button2.TabIndex = 2;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.BtnMenu);
+            this.panel1.Controls.Add(this.BtnHistory);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 40);
+            this.panel1.Margin = new System.Windows.Forms.Padding(0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(350, 40);
+            this.panel1.TabIndex = 2;
+            // 
+            // BtnMenu
+            // 
+            this.BtnMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.BtnMenu.FlatAppearance.BorderSize = 0;
+            this.BtnMenu.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.BtnMenu.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnMenu.Image = ((System.Drawing.Image)(resources.GetObject("BtnMenu.Image")));
+            this.BtnMenu.Location = new System.Drawing.Point(0, 0);
+            this.BtnMenu.Margin = new System.Windows.Forms.Padding(0);
+            this.BtnMenu.Name = "BtnMenu";
+            this.BtnMenu.Size = new System.Drawing.Size(50, 40);
+            this.BtnMenu.TabIndex = 2;
+            this.BtnMenu.UseVisualStyleBackColor = true;
+            // 
+            // BtnHistory
+            // 
+            this.BtnHistory.Dock = System.Windows.Forms.DockStyle.Right;
+            this.BtnHistory.FlatAppearance.BorderSize = 0;
+            this.BtnHistory.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.BtnHistory.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnHistory.Image = ((System.Drawing.Image)(resources.GetObject("BtnHistory.Image")));
+            this.BtnHistory.Location = new System.Drawing.Point(300, 0);
+            this.BtnHistory.Margin = new System.Windows.Forms.Padding(0);
+            this.BtnHistory.Name = "BtnHistory";
+            this.BtnHistory.Size = new System.Drawing.Size(50, 40);
+            this.BtnHistory.TabIndex = 0;
+            this.BtnHistory.UseVisualStyleBackColor = true;
+            // 
+            // TxtDisplay2
+            // 
+            this.TxtDisplay2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.TxtDisplay2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TxtDisplay2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TxtDisplay2.Font = new System.Drawing.Font("Gadugi", 16F);
+            this.TxtDisplay2.ForeColor = System.Drawing.Color.Silver;
+            this.TxtDisplay2.Location = new System.Drawing.Point(0, 80);
+            this.TxtDisplay2.Margin = new System.Windows.Forms.Padding(0);
+            this.TxtDisplay2.Multiline = true;
+            this.TxtDisplay2.Name = "TxtDisplay2";
+            this.TxtDisplay2.Size = new System.Drawing.Size(350, 25);
+            this.TxtDisplay2.TabIndex = 3;
+            this.TxtDisplay2.Text = "811";
+            this.TxtDisplay2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // TxtDisplay1
+            // 
+            this.TxtDisplay1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.TxtDisplay1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TxtDisplay1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TxtDisplay1.Font = new System.Drawing.Font("Gadugi", 32F, System.Drawing.FontStyle.Bold);
+            this.TxtDisplay1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.TxtDisplay1.Location = new System.Drawing.Point(0, 105);
+            this.TxtDisplay1.Margin = new System.Windows.Forms.Padding(0);
+            this.TxtDisplay1.Multiline = true;
+            this.TxtDisplay1.Name = "TxtDisplay1";
+            this.TxtDisplay1.Size = new System.Drawing.Size(350, 50);
+            this.TxtDisplay1.TabIndex = 4;
+            this.TxtDisplay1.Text = "811";
+            this.TxtDisplay1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // BtnClearHistory
+            // 
+            this.BtnClearHistory.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.BtnClearHistory.FlatAppearance.BorderSize = 0;
+            this.BtnClearHistory.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.BtnClearHistory.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnClearHistory.Image = ((System.Drawing.Image)(resources.GetObject("BtnClearHistory.Image")));
+            this.BtnClearHistory.Location = new System.Drawing.Point(0, -35);
+            this.BtnClearHistory.Margin = new System.Windows.Forms.Padding(0);
+            this.BtnClearHistory.Name = "BtnClearHistory";
+            this.BtnClearHistory.Size = new System.Drawing.Size(350, 40);
+            this.BtnClearHistory.TabIndex = 3;
+            this.BtnClearHistory.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.ForeColor = System.Drawing.Color.Silver;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Horizontal;
+            this.richTextBox1.Size = new System.Drawing.Size(350, 0);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // artanElipseControl1
+            // 
+            this.artanElipseControl1.CornerRedius = 20;
+            this.artanElipseControl1.TargetControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.ClientSize = new System.Drawing.Size(350, 570);
+            this.Controls.Add(this.TxtDisplay1);
+            this.Controls.Add(this.TxtDisplay2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.PnlHistory);
+            this.Controls.Add(this.PnlTitle);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.PnlTitle.ResumeLayout(false);
+            this.PnlHistory.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PnlTitle;
+        private System.Windows.Forms.Panel PnlHistory;
+        private System.Windows.Forms.Button BtnExit;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button BtnMenu;
+        private System.Windows.Forms.Button BtnHistory;
+        private System.Windows.Forms.TextBox TxtDisplay2;
+        private System.Windows.Forms.TextBox TxtDisplay1;
+        private System.Windows.Forms.Button BtnClearHistory;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private ArtanElipseControl artanElipseControl1;
+    }
+}
+
